47d714f2af97c5682baba62348c71cd511abd8a9
sonvp97 <112803870+sonvp97@users.noreply.github.com>
2022-09-12 17:03:58 +0700

    dd

--- nangcao.js
+++ nangcao.js
@@ -1,32 +1,15 @@
-function bay(){
-   document.getElementById("ketqua").innerHTML=7;
-}
-function tam(){
-    document.getElementById("ketqua").innerHTML=8;
-}
-function chin(){
-    document.getElementById("ketqua").innerHTML=9;
-}
-function sau(){
-    document.getElementById("ketqua").innerHTML=6;
-}
-function nam(){
-    document.getElementById("ketqua").innerHTML=5;
-}
-function bon(){
-    document.getElementById("ketqua").innerHTML=4;
-}
-function ba(){
-    document.getElementById("ketqua").innerHTML=3;
-}
-function hai(){
-    document.getElementById("ketqua").innerHTML=2;
-}
-function mot(){
-    document.getElementById("ketqua").innerHTML=1;
-}
-function khong(){
-    document.getElementById("ketqua").innerHTML=0;
-}
-
+let input=document.querySelector('.input');
+let items=Array.from(document.querySelectorAll('.item'));
+items.forEach(function(btn){
+    btn.addEventListener('click',function(){
+        if(input.innerHTML == '0')
+            input.innerHTML='';
+        if(btn.innerHTML=='AC')
+            input.innerHTML='0';
+        else if(btn.innerHTML=='='){
+            input.innerHTML=eval(input.innerHTML);
+        }
+        else input.innerHTML+=btn.innerHTML;
+    })
+})
 
